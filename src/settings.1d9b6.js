e7e92c036919168d47fce1b91ad71c1c8c05017d
AnarkiXD <siso7667@gmail.com>
2022-08-10 17:28:27 +0800

    A

---
+++ src/settings.1d9b6.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/resources/Scenes/Loading.fire",orientation:"landscape",jsList:["assets/Script/Libraries/CryptoJS/core-min.dda48.js"],bundleVers:{resources:"c9b20",internal:"d17e2",main:"3e514"}};
--- src/settings.998a8.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/resources/Scenes/Loading.fire",orientation:"landscape",jsList:["assets/Script/Libraries/CryptoJS/core-min.dda48.js"],bundleVers:{resources:"a5a0b",internal:"d17e2",main:"f9bf7"}};
